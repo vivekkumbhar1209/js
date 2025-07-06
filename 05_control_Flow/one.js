9626807d3add69090ded4d6709da96e0c4f27e56
unknown <vivekkumbhar3310@gmail.com>
2025-07-06 10:42:32 +0530

    if else statement

---
+++ 05_control_Flow/one.js
@@ -0,0 +1,48 @@
+// const isUserloggedaIn = true 
+// const temperature = 41
+
+// if(temperature < 50) {
+//     console.log("less than 50");
+// }
+// console.log("greater than 50");
+
+// *************************************************88888
+
+// const score = 200;
+
+// if(score > 100) {
+//     const power = "super" // var use kele tar two console execute honar
+//     console.log(`user power : ${power}`);
+// }
+
+// console.log(`user power : ${power}`);
+
+// const balance = 1000
+
+//if (balance > 500 ) console.log("test"), console.log("test2");  //do not write this code in company  
+
+// if (balance < 500) {
+//     console.log("balance is less than 500");
+// } 
+// else if (balance < 750){
+//     console.log("balance is less than 750");
+// }
+// else 
+// {
+//     console.log("balance is less than 1200");
+// }
+
+
+
+const isUserLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogel = false;
+const loggedInFromEmail = true; 
+
+if (isUserLoggedIn && debitCard && 2==3) {
+    console.log("Allow for cource");
+}
+if (loggedInFromGoogel || loggedInFromEmail) {
+  console.log("User log in");   
+}
+
