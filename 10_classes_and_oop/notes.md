b5c4567dbd7630bd61219d1e6e8d98c01f2eaf31
unknown <vivekkumbhar3310@gmail.com>
2025-07-24 20:07:37 +0530

    opps basic

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,27 @@
+# JavaScript and classes
+# there is no classes in js.
+
+## 1. OOPS 
+
+## Object
+- collection of properties and methods
+- toLowerCase
+
+## why use OOP 
+
+## parts of OOP
+Object literal
+
+- Constructor function
+- prototypes
+- Classes
+- Instances (new, this)
+
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
+
+
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,37 @@
+const user = {
+    username : "Vivek",
+    loginCount : 8,
+    signedIn : true,
+
+    getUserDetails: function () {
+        // console.log("Get User Details from database");
+       // console.log(`Username: ${this.username}` );
+       console.log(this);
+    }
+    
+        
+}
+// console.log(user.username);
+//console.log(user.getUserDetails());
+// console.log(this);
+
+// #####################  constructor function ##############3
+
+function User(username, loginCount, isloggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isloggedIn = isloggedIn;
+
+   this.greeting = function ()
+{
+    console.log(`Hello ${this.username}`);
+}    
+    return this;
+}
+
+const user1 = new User("Vivek", 8, true);
+const user2 = new User("John",5 , false);
+console.log(user1.constructor);
+console.log(user1);
+//console.log(user2);
+
