35c27e66f025280e0154e89882e40f3178bcf097
unknown <vivekkumbhar3310@gmail.com>
2025-07-27 13:20:17 +0530

    call and this

---
+++ 10_classes_and_oop/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username){
+    // complex DB call
+    this.username = username;
+}
+
+function createUSer(username, email, password){
+    SetUsername.call(this, username)
+    //console.log("called")
+    
+    this.email = email;
+    this.password = password;
+}
+
+const chai = new createUSer("chai", "chai@123.com", "123")
+console.log(chai)
